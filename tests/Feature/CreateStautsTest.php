e6a04b685f2988f7e60aa1635b4a318e34dbfc67
Pablo Vieyra <vieyrapez@gmail.com>
2019-04-10 13:27:20 -0500

    Tests urls

--- tests/Feature/CreateStautsTest.php
+++ tests/Feature/CreateStautsTest.php
@@ -8,17 +8,36 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class CreateStautsTest extends TestCase {
-    /**
+
+	use RefreshDatabase;
+	/**
      @test
      **/
 
+		public function guests_users_can_not_create_statuses(){
+			
+			$response = $this->post(route('statuses.store'), ['body'=>'mi primer status']);
+			//dd($response->content());
+			$response->assertRedirect('login');
+		}
+
     public function an_authenticated_user_can_create_statuses(){
+
+			$this->withoutExceptionHandling();
 			#1. Given Teniendo un usuario autenticado.
 			$user = factory(User::class)->create(['email'=>'vieyrapez@gmail.com']);
 			$this->actingAs($user);
 			#2. when Cuando hace un post request a status.
-			$this->post(route('status.store'), ['body'=>'mi primer status']);
+			$response = $this->post(route('statuses.store'), ['body'=>'mi primer status']);
+			$response->assertJson([
+				'body'=> 'mi primer status'
+			]);
+
+
 			#3. Then Entonces veo un nuevo estado en la base de datos.
-			$this->assertTrue(true);
+			$this->assertDatabaseHas('statuses',[
+				'user_id' => $user->id,
+				'body' => 'mi primer status'
+			]);
     }
 }
